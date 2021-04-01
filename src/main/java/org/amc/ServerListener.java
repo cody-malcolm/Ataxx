a057af66641261c38d85e58974395ba0136e4eaf
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-03-31 23:47:48 -0400

    Implement max # of clients

--- src/main/java/org/amc/ServerListener.java
+++ src/main/java/org/amc/ServerListener.java
@@ -12,10 +12,14 @@ import java.net.SocketException;
 public class ServerListener extends Thread{
     /** The port the server will listen to */
     final private static int PORT = 25436;
+    /** The max number of simultaneous connections */
+    final private static int MAX_CLIENTS = 100; // TODO test this with 2 later on
     /** The Socket used to facilitate the connection */
     private ServerSocket serverSocket = null;
     /** A flag to indicate if a stop has been requested */
     private boolean stopped;
+    /** The Players (Threads) connected to the server */
+    private Player[] players = null;
 
     /**
      * Constructor for ListenerThread.
@@ -39,17 +43,24 @@ public class ServerListener extends Thread{
 
             // print a message indicating that the server is listening for requests
             System.out.println("The server is listening to port: " + PORT);
-
+            players = new Player[MAX_CLIENTS];
+            int numPlayers = 0;
             while (!stopped) {
                 try {
                     // wait for a connection
                     Socket clientSocket = serverSocket.accept();
 
+                    // TODO "Server is at max capacity" message if all are active
+                    while (null != players[numPlayers] && players[numPlayers].isAlive()) {
+                        numPlayers++;
+                    }
+
                     // create a Thread for the new connection
-                    Player player = new Player(clientSocket);
+                    players[numPlayers] = new Player(clientSocket);
 
                     // start the thread
-                    player.start();
+                    players[numPlayers++].start();
+                    numPlayers %= MAX_CLIENTS;
                 } catch(SocketException e) {
                     // Happens when thread is interrupted. Do nothing.
                 }
